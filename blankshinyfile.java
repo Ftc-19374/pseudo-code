fe04b9a3030a683afc1f22aea176e0ba6ec2e552
harrythegamer190 <87872383+harrythegamer190@users.noreply.github.com>
2021-11-06 09:45:30 -0400

    Create blankshinyfile.java

---
+++ blankshinyfile.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode;
+
+// import autonomous (or teleop)
+// import dcmotor controller
+
+public class blankshinyfile {
+    /* 
+    set variables of circumference of wheel
+    set encoder #
+    
+    create motor
+    activate motor
+    set to run using encoders
+    
+    
+    
+    find how much a robot will move in a rotation
+
+    move up 100 ticks
+    move down 100 ticks
+    
+    add dat telemetry data cause why not
+
+    end program
+     */
+}
