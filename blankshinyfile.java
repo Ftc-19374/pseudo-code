65481087776fec3f9d8f6faf1fee4f2e343cbf52
ftc19374 <86372652+ftc19374@users.noreply.github.com>
2021-11-08 18:02:31 -0500

    Update blankshinyfile.java

--- blankshinyfile.java
+++ blankshinyfile.java
@@ -2,25 +2,32 @@ package org.firstinspires.ftc.teamcode;
 
 // import autonomous (or teleop)
 // import dcmotor controller
-
+@Autonomous(name="Encoder movement")
 public class blankshinyfile {
-    /* 
+    /*
     set variables of circumference of wheel
     set encoder #
-    
     create motor
     activate motor
     set to run using encoders
-    
-    
-    
     find how much a robot will move in a rotation
-
-    move up 100 ticks
-    move down 100 ticks
+    */
     
-    add dat telemetry data cause why not
-
-    end program
-     */
+    @Override
+    public void runOpMode() throws InterruptedException {
+        waitForStart();
+        /* 
+        move up 100 ticks (call void)
+        move down 100 ticks (call void)
+        */
+        while(opModeIsActive()){
+            // Add telemetry data
+        }
+    }
+    
+    public void movingvoid(Parameters){
+        /*
+        complex calculations (you shouldn't need to edit this, as it will be used in all programs and pretested. If the problem comes down to this, you can edit this.)
+        */
+    }
 }
