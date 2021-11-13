06524296dfd71b1caec46135b6e3c7388117ee58
ftc19374 <86372652+ftc19374@users.noreply.github.com>
2021-11-13 08:34:37 -0500

    Update blankshinyfile.java

--- blankshinyfile.java
+++ blankshinyfile.java
@@ -10,30 +10,29 @@ on user input from controller 1, with the a and b buttons being pressed, althoug
 // This class name should be the same as the name of the file. That might be why there is an error compiling.
 public class blankshinyfile {
     /*
-    set variables of tick number of a gobilda motor (it's in here because we need to know how far we can move with a rotation.)
+    set variables of tick number of a gobilda 5202/5203 motor (it's in here because we need to know how far we can move with a rotation.)
     set encoder #
     create cascading lift motor (it's the only one)
-    activate motor
     create button variables
-    set to run using encoders
     find how much a robot will move in a rotation
     */
     
     @Override
     public void runOpMode() throws InterruptedException {
         //Declare motor from hardware map
+        //set to run using encoders
         waitForStart();
         while(opModeIsActive()){
             // Add telemetry data of how far we're moving (just in case)
-            // Set button values
+            // Set button values based on input from controller
             //if a button is pressed:
-            //    move down 100 ticks (by using movingvoid)
+            //    movingvoid(Distance: -100 ticks, Speed: 0.5. Timeout: 5)
             //if b button is pressed:
-            //    move up 100 ticks (by using movingvoid)
+            //    movingvoid(Distance: 100 ticks, Speed: 0.5, Timeout: 5)
         }
     }
     
-    public void movingvoid(Parameters){
+    public void movingvoid(double Distance, double Speed, double Timeout){
         /*
         complex calculations (you shouldn't need to edit this, as it will be used in all programs and pretested. If the problem comes down to this, you can edit this.)
         */
